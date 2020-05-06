a99d640483f217304b5cdf5a607b2e28a9fc2c20
jiwonan <s2018s31@e-mirim.hs.kr>
2020-05-06 15:16:23 +0900

    2020.05.06 C# 실습 문제 03

--- cscomment/Program.cs
+++ cscomment/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,25 @@ namespace cscomment
     {
         static void Main(string[] args)
         {
+            int x, y;
+
+            /*
+            Console.Write("x값 입력: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("y값 입력: ");
+            y = int.Parse(Console.ReadLine());
+            */
+            if (x > 4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
